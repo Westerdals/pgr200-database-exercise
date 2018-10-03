bb2829ceac3193f5c85b994c303014feee558732
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2018-10-03 20:10:08 +0200

    Remove used imports

--- src/main/java/no/kristiania/pgr200/database/ConferenceTalkDao.java
+++ src/main/java/no/kristiania/pgr200/database/ConferenceTalkDao.java
@@ -1,7 +1,5 @@
 package no.kristiania.pgr200.database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
@@ -15,20 +13,8 @@ public class ConferenceTalkDao {
     }
 
     public void createTableIfNotExists() throws SQLException {
-        try (Connection conn = dataSource.getConnection()) {
-            conn.createStatement().executeUpdate("create table if not exists CONFERENCE_TALK (TITLE varchar primary key, DESCRIPTION text)");
-        }
     }
 
     public void insertTalk(String title, String description) throws SQLException {
-        try(Connection conn = dataSource.getConnection()) {
-            String sql = "insert into CONFERENCE_TALK (TITLE, DESCRIPTION) values (?, ?)";
-            try (PreparedStatement statement = conn.prepareStatement(sql)) {
-                statement.setString(1, title);
-                statement.setString(2, description);
-
-                statement.executeUpdate();
-            }
-        }
     }
 }
